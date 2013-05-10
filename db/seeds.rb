7abc0603eaae59ef6ef4763516c8a69cc64a9ffd
dbcapprentice <dbcapprentice@gmail.com>
2013-05-10 15:44:33 -0700

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # description            = Faker::Name.name
 # complete_status          = Faker::Internet.email
-
+require 'faker'
  
 1000.times do
   description              = Faker::Name.name
-  complete_status          = Faker::Internet.email
+  complete_status          = [true, false][rand(2)]
+  Task.create!(:description => description, :complete_status => complete_status)
 end
